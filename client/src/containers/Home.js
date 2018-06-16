f5b7f698b8147507e3b72aa025e6e56c16e75751
Your Name <mine_wongteeravit@hotmail.com>
2018-06-16 22:18:38 +0700

    add some bgcontent

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -3,12 +3,18 @@ import './Home.css';
 import FieldGroup from '../components/FieldGroup';
 import Button from '../components/Button';
 import logo from '../img/logo.svg';
+import BodyClassName from 'react-body-classname';
 
 class Home extends Component {
     render() {
         return (
-            <div>
-            </div>
+            <BodyClassName className="home-body">
+                <div className="text-name-project">
+                โครงการ เศรษฐสิริ ชัยพฤกษ์-แจ้งวัฒนะ
+                </div>
+            
+            </BodyClassName>
+
         )
     }
 }
