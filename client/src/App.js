d8cf5c542e3e293ba727cfc5fc846fff5dcb3060
Nitipat Wuttisasiwat <k-e-n_ken@hotmail.com>
2018-06-16 10:56:09 +0700

    bootstrap add in html

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import { Link, withRouter } from "react-router-dom";
 import Routes from './Routes';
+// or
+import { Button } from 'react-bootstrap';
 
 class App extends Component {
   constructor(props) {
@@ -18,6 +20,14 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
+        {/* <Button /> */}
+        {/* <ReactMic
+          record={true}         // defaults -> false.  Set to true to begin recording
+          className="ReactMic"       // provide css class name
+          onStop={() => {console.log('Stop')}}        // callback to execute when audio stops recording
+          strokeColor="#000000"     // sound wave color
+          backgroundColor="#FF4081" // background color
+        /> */}
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         {/* <Routes /> */}
