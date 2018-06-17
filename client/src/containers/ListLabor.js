3aca8e6aea4f0e44f33e595b2deb09c7f3fb940a
Nitipat Wuttisasiwat <k-e-n_ken@hotmail.com>
2018-06-17 10:34:11 +0700

    filter listLabor

--- client/src/containers/ListLabor.js
+++ client/src/containers/ListLabor.js
@@ -9,10 +9,42 @@ class NewLabor6 extends Component {
         super(props);
         this.state = {
             labors: [
-
-            ]
+                {
+                    id: '552872',
+                    name: 'อัชราภา สิ่งท้อ',
+                    follower: 2
+                },
+                {
+                    id: '557877',
+                    name: 'ศรีชาย ม่วงมัน',
+                    follower: 1
+                },
+                {
+                    id: '557850',
+                    name: 'แก้วตา ขวัญใจ',
+                    follower: 0
+                },
+                {
+                    id: '573937',
+                    name: 'สมัครใจ ทำงาน',
+                    follower: 1
+                },
+                {
+                    id: '458799',
+                    name: 'มฤตวงศ์ สมสมร',
+                    follower: 1
+                }
+            ],
+            search: ''
         }
     }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    }
+
     render() {
         return (
             <BodyClassName className="newlabor-body">
@@ -23,7 +55,7 @@ class NewLabor6 extends Component {
 
                     <div className="register-box">
                         <div className="search-box">
-                            <input className="search-form" type="text" placeholder="ค้นหา" />
+                            <input id="search" onChange={this.handleChange} value={this.state.search} className="search-form" type="text" placeholder="ค้นหา" />
                             <img className="img-search" src={search} />
                         </div>
                         <table id="customers" className="customers2">
@@ -32,38 +64,20 @@ class NewLabor6 extends Component {
                                 <th>ชื่อแรงงาน</th>
                                 <th className="number-follow-each">จำนวนผู้้ติดตาม</th>
                             </tr>
-                            <tr>
-                                <td>1</td>
-                                <td>นิติพัฒน์ วุฒิศศิวัตน์
-                                <img className="img-profile" onClick={ () => {this.props.history.push('/Info')} } src={profile}/>
-                                </td>
-                                <td>1</td>
-                            </tr>
-                            <tr>
-                                <td>1</td>
-                                <td>นิติพัฒน์ วุฒิศศิวัตน์  <img className="img-profile" onClick={ () => {this.props.history.push('/Info')} } src={profile}/></td>
-                                <td>1</td>
-                            </tr>
-                            <tr>
-                                <td>1</td>
-                                <td>นิติพัฒน์ วุฒิศศิวัตน์  <img className="img-profile" onClick={ () => {this.props.history.push('/Info')} } src={profile}/></td>
-                                <td>1</td>
-                            </tr>
-                            <tr>
-                                <td>1</td>
-                                <td>นิติพัฒน์ วุฒิศศิวัตน์  <img className="img-profile" onClick={ () => {this.props.history.push('/Info')} } src={profile}/></td>
-                                <td>1</td>
-                            </tr> <tr>
-                                <td>1</td>
-                                <td>นิติพัฒน์ วุฒิศศิวัตน์  <img className="img-profile" onClick={ () => {this.props.history.push('/Info')} } src={profile}/></td>
-                                <td>1</td>
-                            </tr>
-
+                            { this.state.labors.filter( labor => {
+                                return labor.id.includes(this.state.search) || labor.name.includes(this.state.search)
+                            }).map( labor => {
+                                return (
+                                    <tr>
+                                        <td>{labor.id}</td>
+                                        <td>    {labor.name}
+                                            <img className="img-profile" onClick={ () => {this.props.history.push('/Info')} } src={profile}/>
+                                        </td>
+                                        <td>{labor.follower}</td>
+                                    </tr>
+                                )
+                            })}
                         </table>
-
-                        {/* // <div className="next-btn">
-                        //     <span className="set-center">ถัดไป</span>
-                        // </div> */}
                     </div>
 
 
